81d80363903611e7f8748f3e50776e82f89c38f9
unknown <yassine59958@gmail.com>
2025-08-26 13:11:29 +0200

    3 months filter backend for expenses

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -39,6 +39,19 @@ export const getExpenses = async (req, res) => {
     }
 }
 
+export const getExpensesThreeMonths = async (req, res) => {
+    try{
+        const db = await connection();
+        const query = "SELECT  * FROM expenses WHERE DATE_FORMAT(date, '%Y-%m') >= DATE_FORMAT(CURDATE() - INTERVAL 2 MONTH, '%Y-%m') ORDER BY date desc";
+        const [result] = await db.execute(query);
+
+        res.status(200).json(result);
+    } catch(err) {
+        console.error("Error geting incomes : ", err)
+        res.status(500).json({sucess: false, message: "Servor error"})
+    }
+}
+
 export const insertIncome = async (req, res) => {
     const { amount, source, date } = req.body;
     if(!amount || !source || !date){
--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getIncome, getExpenses, insertIncome, insertExpenses, deleteIncome, deleteExpenses, editIncome, editExpenses, getIncomeThreeMonths } from '../controllers/transactionController.js';
+import { getIncome, getExpenses, insertIncome, insertExpenses, deleteIncome, deleteExpenses, editIncome, editExpenses, getIncomeThreeMonths, getExpensesThreeMonths } from '../controllers/transactionController.js';
 
 const router = express.Router();
 
 router.get('/income', getIncome)
 router.get('/income/three', getIncomeThreeMonths)
 router.get('/expenses', getExpenses)
+router.get('/expenses/three', getExpensesThreeMonths)
 router.post('/income', insertIncome)
 router.post('/expenses', insertExpenses)
 router.delete('/income/:id', deleteIncome)
