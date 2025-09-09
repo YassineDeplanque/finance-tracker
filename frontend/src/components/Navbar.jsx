18af5b66ae611db89243bfb8e7b7d4808fba4edb
Yassine Deplanque <yassine59958@gmail.com>
2025-09-09 19:49:22 +0200

    redirection fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -90,7 +90,7 @@ function Navbar() {
             </li>
             <li>
               <NavLink
-                to="/signin"
+                to="/homedisc"
                 className={({ isActive }) =>
                   isActive
                     ? 'block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0'
