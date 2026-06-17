217acd8369581cd19fde7608672721d26cccf407
Yassine Deplanque <yassine59958@gmail.com>
2026-06-17 14:05:18 +0200

    ssl connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,7 +8,12 @@ export const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
